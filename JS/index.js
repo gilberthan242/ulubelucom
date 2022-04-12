05bb3cfb3222c1fbae45145823541dca25003e3e
gilberthan242 <handayagilbert@gmail.com>
2022-04-12 11:51:04 +0700

    ulubelucom

---
+++ JS/index.js
@@ -0,0 +1,466 @@
+var captionText = document.getElementById("caption");
+        img.onclick = function(){
+            modal.style.display = "block";
+            modalImg.src = this.src;
+            captionText.innerHTML = this.alt;
+            }
+        function onClick(element) {
+            document.getElementById("img01").src = element.src;
+            document.getElementById("modal01").style.display = "block";
+            }
+
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+
+// modal2 windows for product
+  function left() {
+    if (document.getElementById('left').onclick) {
+        document.getElementById('right').style.display = 'block';
+        document.getElementById('left').style.display = 'none';
+    }
+    }
+  
+    function right() {
+    if (document.getElementById('right').onclick) {
+        document.getElementById('left').style.display = 'block';
+        document.getElementById('right').style.display = 'none';
+    }
+    }
+
+  var captionText = document.getElementById("caption");
+      img.onclick = function(){
+          modal.style.display = "block";
+          modalImg.src = this.src;
+          captionText.innerHTML = this.alt;
+          }
+      function onClick2(element) {
+          document.getElementById("img02").src = element.src;
+          document.getElementById("modal02").style.display = "block";
+          }
+
+  function allright() {
+      onClick2(myImg03);
+      right();
+  }
+
+  function allleft() {
+      onClick2(myImg02);
+      left();
+  }
+
+
+
+
+// modal3 windows for product
+function left1() {
+  if (document.getElementById('left1').onclick) {
+      document.getElementById('right1').style.display = 'block';
+      document.getElementById('left1').style.display = 'none';
+  }
+  }
+
+  function right1() {
+  if (document.getElementById('right1').onclick) {
+      document.getElementById('left1').style.display = 'block';
+      document.getElementById('right1').style.display = 'none';
+  }
+  }
+
+var captionText = document.getElementById("caption");
+    img.onclick = function(){
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        captionText.innerHTML = this.alt;
+        }
+    function onClick3(element) {
+        document.getElementById("img03").src = element.src;
+        document.getElementById("modal03").style.display = "block";
+        }
+
+function allright1() {
+    onClick3(myImg06);
+    right1();
+}
+
+function allleft1() {
+    onClick3(myImg05);
+    left1();
+}
+
+
+
+
+// modal4 windows for product
+function left2() {
+  if (document.getElementById('left2').onclick) {
+      document.getElementById('right2').style.display = 'block';
+      document.getElementById('left2').style.display = 'none';
+  }
+  }
+
+  function right2() {
+  if (document.getElementById('right2').onclick) {
+      document.getElementById('left2').style.display = 'block';
+      document.getElementById('right2').style.display = 'none';
+  }
+  }
+
+var captionText = document.getElementById("caption");
+    img.onclick = function(){
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        captionText.innerHTML = this.alt;
+        }
+    function onClick4(element) {
+        document.getElementById("img04").src = element.src;
+        document.getElementById("modal04").style.display = "block";
+        }
+
+function allright2() {
+    onClick4(myImg09);
+    right2();
+}
+
+function allleft2() {
+    onClick4(myImg08);
+    left2();
+}
+
+
+
+// modal5 windows for product
+function left3() {
+  if (document.getElementById('left3').onclick) {
+      document.getElementById('right3').style.display = 'block';
+      document.getElementById('left3').style.display = 'none';
+  }
+  }
+
+  function right3() {
+  if (document.getElementById('right3').onclick) {
+      document.getElementById('left3').style.display = 'block';
+      document.getElementById('right3').style.display = 'none';
+  }
+  }
+
+var captionText = document.getElementById("caption");
+    img.onclick = function(){
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        captionText.innerHTML = this.alt;
+        }
+    function onClick5(element) {
+        document.getElementById("img05").src = element.src;
+        document.getElementById("modal05").style.display = "block";
+        }
+
+function allright3() {
+    onClick5(myImg12);
+    right3();
+}
+
+function allleft3() {
+    onClick5(myImg11);
+    left3();
+}
+
+
+
+
+// modal6 windows for product
+function left4() {
+  if (document.getElementById('left4').onclick) {
+      document.getElementById('right4').style.display = 'block';
+      document.getElementById('left4').style.display = 'none';
+  }
+  }
+
+  function right4() {
+  if (document.getElementById('right4').onclick) {
+      document.getElementById('left4').style.display = 'block';
+      document.getElementById('right4').style.display = 'none';
+  }
+  }
+
+var captionText = document.getElementById("caption");
+    img.onclick = function(){
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        captionText.innerHTML = this.alt;
+        }
+    function onClick6(element) {
+        document.getElementById("img06").src = element.src;
+        document.getElementById("modal06").style.display = "block";
+        }
+
+function allright4() {
+    onClick6(myImg15);
+    right4();
+}
+
+function allleft4() {
+    onClick6(myImg14);
+    left4();
+}
+
+
+
+
+// modal7 windows for product
+function left5() {
+  if (document.getElementById('left5').onclick) {
+      document.getElementById('right5').style.display = 'block';
+      document.getElementById('left5').style.display = 'none';
+  }
+  }
+
+  function right5() {
+  if (document.getElementById('right5').onclick) {
+      document.getElementById('left5').style.display = 'block';
+      document.getElementById('right5').style.display = 'none';
+  }
+  }
+
+var captionText = document.getElementById("caption");
+    img.onclick = function(){
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        captionText.innerHTML = this.alt;
+        }
+    function onClick7(element) {
+        document.getElementById("img07").src = element.src;
+        document.getElementById("modal07").style.display = "block";
+        }
+
+function allright5() {
+    onClick7(myImg18);
+    right5();
+}
+
+function allleft5() {
+    onClick7(myImg17);
+    left5();
+}
+
+
+
+
+// modal8 windows for product
+function left6() {
+  if (document.getElementById('left6').onclick) {
+      document.getElementById('right6').style.display = 'block';
+      document.getElementById('left6').style.display = 'none';
+  }
+  }
+
+  function right6() {
+  if (document.getElementById('right6').onclick) {
+      document.getElementById('left6').style.display = 'block';
+      document.getElementById('right6').style.display = 'none';
+  }
+  }
+
+var captionText = document.getElementById("caption");
+    img.onclick = function(){
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        captionText.innerHTML = this.alt;
+        }
+    function onClick8(element) {
+        document.getElementById("img08").src = element.src;
+        document.getElementById("modal08").style.display = "block";
+        }
+
+function allright6() {
+    onClick8(myImg21);
+    right6();
+}
+
+function allleft6() {
+    onClick8(myImg20);
+    left6();
+}
+
+
+
+
+// modal9 windows for product
+function left7() {
+  if (document.getElementById('left7').onclick) {
+      document.getElementById('right7').style.display = 'block';
+      document.getElementById('left7').style.display = 'none';
+  }
+  }
+
+  function right7() {
+  if (document.getElementById('right7').onclick) {
+      document.getElementById('left7').style.display = 'block';
+      document.getElementById('right7').style.display = 'none';
+  }
+  }
+
+var captionText = document.getElementById("caption");
+    img.onclick = function(){
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        captionText.innerHTML = this.alt;
+        }
+    function onClick9(element) {
+        document.getElementById("img09").src = element.src;
+        document.getElementById("modal09").style.display = "block";
+        }
+
+function allright7() {
+    onClick9(myImg24);
+    right7();
+}
+
+function allleft7() {
+    onClick9(myImg23);
+    left7();
+}
+
+
+
+
+// modal10 windows for product
+function left8() {
+  if (document.getElementById('left8').onclick) {
+      document.getElementById('right8').style.display = 'block';
+      document.getElementById('left8').style.display = 'none';
+  }
+  }
+
+  function right8() {
+  if (document.getElementById('right8').onclick) {
+      document.getElementById('left8').style.display = 'block';
+      document.getElementById('right8').style.display = 'none';
+  }
+  }
+
+var captionText = document.getElementById("caption");
+    img.onclick = function(){
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        captionText.innerHTML = this.alt;
+        }
+    function onClick10(element) {
+        document.getElementById("img010").src = element.src;
+        document.getElementById("modal010").style.display = "block";
+        }
+
+function allright8() {
+    onClick10(myImg27);
+    right8();
+}
+
+function allleft8() {
+    onClick10(myImg26);
+    left8();
+}
+
+
+
+
+// modal11 windows for product
+function left9() {
+  if (document.getElementById('left9').onclick) {
+      document.getElementById('right9').style.display = 'block';
+      document.getElementById('left9').style.display = 'none';
+  }
+  }
+
+  function right9() {
+  if (document.getElementById('right9').onclick) {
+      document.getElementById('left9').style.display = 'block';
+      document.getElementById('right9').style.display = 'none';
+  }
+  }
+
+var captionText = document.getElementById("caption");
+    img.onclick = function(){
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        captionText.innerHTML = this.alt;
+        }
+    function onClick11(element) {
+        document.getElementById("img011").src = element.src;
+        document.getElementById("modal011").style.display = "block";
+        }
+
+function allright9() {
+    onClick11(myImg30);
+    right9();
+}
+
+function allleft9() {
+    onClick11(myImg29);
+    left9();
+}
+
+
+
+
+// modal12 windows for product
+function left10() {
+  if (document.getElementById('left10').onclick) {
+      document.getElementById('right10').style.display = 'block';
+      document.getElementById('left10').style.display = 'none';
+  }
+  }
+
+  function right10() {
+  if (document.getElementById('right10').onclick) {
+      document.getElementById('left10').style.display = 'block';
+      document.getElementById('right10').style.display = 'none';
+  }
+  }
+
+var captionText = document.getElementById("caption");
+    img.onclick = function(){
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        captionText.innerHTML = this.alt;
+        }
+    function onClick12(element) {
+        document.getElementById("img012").src = element.src;
+        document.getElementById("modal012").style.display = "block";
+        }
+
+function allright10() {
+    onClick12(myImg33);
+    right10();
+}
+
+function allleft10() {
+    onClick12(myImg32);
+    left10();
+}
+
+
+// mini pop up
+function myPop() {
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+  }
